07d605bd2151fb47c3d558743150aa8e34063ee7
hazem82722 <h7n7nv@gmail.com>
2026-04-23 14:30:02 +0300

    Update databaseCars.php

--- php/databaseCars.php
+++ php/databaseCars.php
@@ -8,10 +8,6 @@ if (isset($_POST['buy_submit'])) {
 
     $conn->query("INSERT INTO orders (car_id, full_name) VALUES ('$car_id', '$name')");
 
-<<<<<<< HEAD
-    echo "تم الشراء";
-=======
     echo "Done";
->>>>>>> d9fe65461f25fab10e986dc22ec1d399feea0042
 }
 ?>
