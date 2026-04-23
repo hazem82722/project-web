2ca6bd1fbc83cca901932b6b5bf663fc03e801ac
hazem82722 <h7n7nv@gmail.com>
2026-04-23 15:23:03 +0300

    Update

--- php/databaseCars.php
+++ php/databaseCars.php
@@ -4,14 +4,23 @@ include "db.php";
 if (isset($_POST['buy_submit'])) {
 
     $car_id = $_POST['car_id'];
-    $name = $_POST['full_name'];
+    $car_name = $_POST['car_name'];
+    $car_year = $_POST['car_year'];
+    $full_name = $_POST['full_name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+    $paid_price = $_POST['paid_price'];
 
-    $conn->query("INSERT INTO orders (car_id, full_name) VALUES ('$car_id', '$name')");
+    $sql = "INSERT INTO orders 
+    (car_id, car_name, car_year, full_name, email, phone, address, paid_price)
+    VALUES 
+    ('$car_id', '$car_name', '$car_year', '$full_name', '$email', '$phone', '$address', '$paid_price')";
 
-<<<<<<< HEAD
-    echo "تم الشراء";
-=======
-    echo "Done";
->>>>>>> d9fe65461f25fab10e986dc22ec1d399feea0042
+    if ($conn->query($sql)) {
+        echo "Done";
+    } else {
+        echo "خطأ: " . $conn->error;
+    }
 }
 ?>
