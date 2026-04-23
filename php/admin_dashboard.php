02c0d0a7a4e1e8c926b2627866e277ffb74daad0
hazem82722 <h7n7nv@gmail.com>
2026-04-23 15:24:45 +0300

    Update admin_dashboard.php

--- php/admin_dashboard.php
+++ php/admin_dashboard.php
@@ -8,7 +8,10 @@ $orders = $conn->query("SELECT * FROM orders");
 <h2>Cars</h2>
 <table border="1">
 <tr>
-<th>ID</th><th>Brand</th><th>Model</th>
+<th>ID</th>
+<th>Brand</th>
+<th>Model</th>
+<th>Price</th>
 </tr>
 
 <?php while($c = $cars->fetch_assoc()) { ?>
@@ -16,14 +19,23 @@ $orders = $conn->query("SELECT * FROM orders");
 <td><?php echo $c['ID']; ?></td>
 <td><?php echo $c['Brand']; ?></td>
 <td><?php echo $c['Model']; ?></td>
+<td><?php echo $c['Price']; ?></td>
 </tr>
 <?php } ?>
 </table>
 
+<br><br>
+
 <h2>Orders</h2>
 <table border="1">
 <tr>
-<th>ID</th><th>Car ID</th><th>Name</th>
+<th>ID</th>
+<th>Car ID</th>
+<th>Name</th>
+<th>Email</th>
+<th>Phone</th>
+<th>Address</th>
+<th>Paid</th>
 </tr>
 
 <?php while($o = $orders->fetch_assoc()) { ?>
@@ -31,6 +43,10 @@ $orders = $conn->query("SELECT * FROM orders");
 <td><?php echo $o['id']; ?></td>
 <td><?php echo $o['car_id']; ?></td>
 <td><?php echo $o['full_name']; ?></td>
+<td><?php echo $o['email']; ?></td>
+<td><?php echo $o['phone']; ?></td>
+<td><?php echo $o['address']; ?></td>
+<td><?php echo $o['paid_price']; ?></td>
 </tr>
 <?php } ?>
 </table>
